e193daa55b0bafbab162b10783e0e177943e9fda
krivitskayama <126545157+krivitskayama@users.noreply.github.com>
2023-03-21 11:25:43 +0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,35 @@
+const startSlider=()=>{
+  const sliderItems=document.querySelectorAll(".slider__item"); 
+  const sliderList=document.querySelector(".slider__list");
+  const prevbtn=document.querySelector(".slider__arrow_left");
+  const nextbtn=document.querySelector(".slider__arrow_right");
+  
+  let activeSlide=1;
+  let position=0;
+  
+  const nextSlide=()=>{
+      sliderItems[activeSlide].classList.remove("slider__item_active");
+     
+      position=-sliderItems[0].clientWidth*activeSlide;
+      sliderList.style.transform = `translateX(${position}px)`
+      activeSlide+=1;
+      sliderItems[activeSlide].classList.add("slider__item_active");
+  }
+  const prevSlide=()=>{
+      sliderItems[activeSlide].classList.remove("slider__item_active");
+     
+      position=sliderItems[0].clientWidth*activeSlide;
+      sliderList.style.transform = `translateX(${position}px)`
+      activeSlide-=1;
+      sliderItems[activeSlide].classList.add("slider__item_active");
+  }
+  prevbtn.addEventListener("click",prevSlide);
+  nextbtn.addEventListener("click",nextSlide);
+};
+
+document.addEventListener("DOMContentLoaded",startSlider)
+
+
+
+
+ 
---
+++ js/preload.js
@@ -0,0 +1,5 @@
+const addPreload=(elem)=>{
+  elem.classList.add("preload");
+};
+const preload=document.querySelector("body");
+addPreload(preload);
