3568ac4754420aa5c412e0aa8e25b72f3ced7e45
krivitskayama <126545157+krivitskayama@users.noreply.github.com>
2023-03-22 10:46:45 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-const startSlider=()=>{
+  const startSlider=()=>{
   const sliderItems=document.querySelectorAll(".slider__item"); 
   const sliderList=document.querySelector(".slider__list");
   const prevbtn=document.querySelector(".slider__arrow_left");
@@ -8,29 +8,28 @@ const startSlider=()=>{
   let position=0;
   
   const nextSlide=()=>{
-    if (activeSlide==4){
+   
+    if (activeSlide<3){
       sliderItems[activeSlide].classList.remove("slider__item_active");
      
       position=-sliderItems[0].clientWidth*activeSlide;
       sliderList.style.transform = `translateX(${position}px)`
       activeSlide+=1;
-      sliderItems[activeSlide].classList.add("slider__item_active");};
+      sliderItems[activeSlide].classList.add("slider__item_active");}
+
   }
   const prevSlide=()=>{
+
+    if (activeSlide>1){
       sliderItems[activeSlide].classList.remove("slider__item_active");
      
-      position=sliderItems[0].clientWidth*activeSlide;
+      position=-sliderItems[0].clientWidth*(activeSlide-2);
       sliderList.style.transform = `translateX(${position}px)`
       activeSlide-=1;
-      sliderItems[activeSlide].classList.add("slider__item_active");
+      sliderItems[activeSlide].classList.add("slider__item_active");} 
   }
   prevbtn.addEventListener("click",prevSlide);
   nextbtn.addEventListener("click",nextSlide);
 };
 
 document.addEventListener("DOMContentLoaded",startSlider)
-
-
-
-
- 
